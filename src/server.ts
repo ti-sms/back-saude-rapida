ec0f1c2bdff0c5943b17a6f6ae4bc4b1fc96475f
thaliaEllenAraujo <thaliaellenaraujo@gmail.com>
2025-01-23 17:02:22 -0300

    [SRD-73][CONFIG] Folder hierarchy for poo

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
-import express from "express";
+import express, { Router } from "express";
+import cors from "cors";
 
 const app = express();
+const router = Router();
 
-const logger = (req: any, res: any, next: any) => {
-    console.log(`Request method? ${req.method}, URL: ${req.url}`);
-};
+app.use(express.json({limit: "10mb"}));
+app.use(express.urlencoded({limit: "10mb", extended: true, parameterLimit: 50000}));
 
-app.use(logger);
+app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("Hello world 🌞");
-});
-
-export default app;  
+export default app;     
